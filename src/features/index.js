38dea1cc855d8cf36d8069694a0d169e497f13ee
Paweł <pawelnackowski@gmail.com>
2023-06-21 20:54:15 +0200

    created quiz component

---
+++ src/features/index.js
@@ -0,0 +1,10 @@
+import {Tile} from "../common/Tile";
+import {Position} from "./styled";
+
+export const Quiz = () => {
+    return (
+        <Position>
+            <Tile title={"Quiz App"}/>
+        </Position>
+    )
+}
