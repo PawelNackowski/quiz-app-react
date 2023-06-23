9c3bc11944263dfc267d78aa8ddfe51ebd523d13
Paweł <pawelnackowski@gmail.com>
2023-06-23 11:11:25 +0200

    added paination and questions wiew

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -1,16 +1,67 @@
-import {Answers, Header, Wrapper} from "./styled";
-import {Pagination} from "./Pagination";
+import { useState } from "react";
+import { Answers, Header, Wrapper } from "./styled";
+import { questions } from "../../features/answers";
 
-export const Tile = ({title}) => {
-    return (
-        <Wrapper>
-            <Header>{title}</Header>
-            <Answers>
-                <input type="checkbox" value="1"/>pierwsza odpowiedź
-                <input type="checkbox" value="2"/>druga odpowiedź
-                <input type="checkbox" value="3"/>trzecia odpowiedź
-            </Answers>
-            <Pagination/>
-        </Wrapper>
-    )
-}
+export const Tile = ({ title }) => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    setSelectedAnswer(null);
+  };
+
+  const handlePreviousQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+    setSelectedAnswer(null);
+  };
+
+  const handleAnswerChange = (event) => {
+    setSelectedAnswer(event.target.value);
+  };
+
+  const handleSend = () => {
+    console.log("Selected Answer:", selectedAnswer);
+  };
+
+  const { id, title: currentQuestionTitle, answers } = questions[currentQuestionIndex];
+
+  const isSendDisabled = selectedAnswer === null;
+
+  return (
+    <Wrapper>
+      <Header>{title}</Header>
+      <Answers>
+        <div key={id}>
+          <h3>{currentQuestionTitle}</h3>
+          <ul>
+            {answers.map(({ id, name, value }) => (
+              <li key={id}>
+                <label>
+                  <input
+                    type="radio"
+                    name={`answer-${id}`}
+                    value={value}
+                    checked={selectedAnswer === value}
+                    onChange={handleAnswerChange}
+                  />
+                  {name}
+                </label>
+              </li>
+            ))}
+          </ul>
+          <button onClick={handleSend} disabled={isSendDisabled}>Send</button>
+        </div>
+      </Answers>
+      <button onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
+        Back
+      </button>
+      <span>
+        {currentQuestionIndex + 1}/{questions.length}
+      </span>
+      <button onClick={handleNextQuestion} disabled={isSendDisabled || currentQuestionIndex === questions.length - 1}>
+        Next
+      </button>
+    </Wrapper>
+  );
+};
