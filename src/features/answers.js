b403d66e06b1f13886b243bd97b0c06be764b907
Paweł <pawelnackowski@gmail.com>
2023-06-23 11:10:31 +0200

    added example questions

---
+++ src/features/answers.js
@@ -0,0 +1,80 @@
+export const questions = [
+    {
+        id: 1,
+        title: "1. What is the capital of Poland?",
+        answers: [
+            {
+                id: 1,
+                name: "Warsaw",
+                value: "Warsaw",
+            },
+            {
+                id: 2,
+                name: "Cracow",
+                value: "Cracow",
+            },
+            {
+                id: 3,
+                name: "Gdansk",
+                value: "Gdansk",
+            },
+            {
+                id: 4,
+                name: "Wroclaw",
+                value: "Wroclaw",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "2. What is the capital of Germany?",
+        answers: [
+            {
+                id: 1,
+                name: "Berlin",
+                value: "Berlin",
+            },
+            {
+                id: 2,
+                name: "Munich",
+                value: "Munich",
+            },
+            {
+                id: 3,
+                name: "Hamburg",
+                value: "Hamburg",
+            },
+            {
+                id: 4,
+                name: "Frankfurt",
+                value: "Frankfurt",
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "3. What is the capital of France?",
+        answers: [
+            {
+                id: 1,
+                name: "Paris",
+                value: "Paris",
+            },
+            {
+                id: 2,
+                name: "Lyon",
+                value: "Lyon",
+            },
+            {
+                id: 3,
+                name: "Marseille",
+                value: "Marseille",
+            },
+            {
+                id: 4,
+                name: "Toulouse",
+                value: "Toulouse",
+            },
+        ],
+    },
+    ];
