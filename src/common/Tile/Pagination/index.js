c03681dccde09af7bbb9ae77143aff05a2cce5f5
Paweł <pawelnackowski@gmail.com>
2023-06-24 09:56:03 +0200

    added pagination component

--- src/common/Tile/Pagination/index.js
+++ src/common/Tile/Pagination/index.js
@@ -1,9 +1,41 @@
-export const Pagination = () => {
+import { questions } from '../../../features/answers'
+
+export const Pagination = ({
+    setCurrentQuestionIndex,
+    setSelectedAnswer,
+    currentQuestionIndex,
+    isSendDisabled,
+}) => {
+    const handleNextQuestion = () => {
+        setCurrentQuestionIndex((prevIndex) => prevIndex + 1)
+        setSelectedAnswer(null)
+    }
+
+    const handlePreviousQuestion = () => {
+        setCurrentQuestionIndex((prevIndex) => prevIndex - 1)
+        setSelectedAnswer(null)
+    }
+
     return (
         <>
-            <button>back</button>
-            1/6
-            <button>next</button>
+            <button
+                onClick={handlePreviousQuestion}
+                disabled={currentQuestionIndex === 0}
+            >
+                Back
+            </button>
+            <span>
+                {currentQuestionIndex + 1}/{questions.length}
+            </span>
+            <button
+                onClick={handleNextQuestion}
+                disabled={
+                    isSendDisabled ||
+                    currentQuestionIndex === questions.length - 1
+                }
+            >
+                Next
+            </button>
         </>
     )
 }
--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -1,72 +1,67 @@
-import { useState } from "react";
-import { Answers, Header, Wrapper } from "./styled";
-import { questions } from "../../features/answers";
+import { useState } from 'react'
+import { Answers, Header, Wrapper } from './styled'
+import { questions } from '../../features/answers'
+import { Pagination } from './Pagination'
 
 export const Tile = ({ title }) => {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+    const [selectedAnswer, setSelectedAnswer] = useState(null)
 
-  const handleNextQuestion = () => {
-    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-    setSelectedAnswer(null);
-  };
+    const handleAnswerChange = (event) => {
+        setSelectedAnswer(event.target.value)
+    }
 
-  const handlePreviousQuestion = () => {
-    setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
-    setSelectedAnswer(null);
-  };
+    const checkAnswer = (answer) => {
+        return answer === questions[currentQuestionIndex].correctAnswer
+            ? '1'
+            : '0'
+    }
+    const handleSend = () => {
+        console.log('Selected Answer:', checkAnswer(selectedAnswer))
+    }
 
-  const handleAnswerChange = (event) => {
-    setSelectedAnswer(event.target.value);
-  };
+    const {
+        id,
+        title: currentQuestionTitle,
+        answers,
+    } = questions[currentQuestionIndex]
 
-  const checkAnswer = (answer) => {
+    const isSendDisabled = selectedAnswer === null
+
     return (
-        answer === questions[currentQuestionIndex].correctAnswer ? "1" : "0"
-    );
-  }
-  const handleSend = () => {
-    console.log("Selected Answer:", checkAnswer(selectedAnswer));
-  };
-
-  const {id, title: currentQuestionTitle, answers} = questions[currentQuestionIndex];
-
-  const isSendDisabled = selectedAnswer === null;
-
-  return (
-      <Wrapper>
-        <Header>{title}</Header>
-        <Answers>
-          <div key={id}>
-            <h3>{currentQuestionTitle}</h3>
-            <ul>
-              {answers.map(({id, name, value}) => (
-                  <li key={id}>
-                    <label>
-                      <input
-                          type="radio"
-                          name={`answer-${id}`}
-                          value={value}
-                          checked={selectedAnswer === value}
-                          onChange={handleAnswerChange}
-                      />
-                      {name}
-                    </label>
-                  </li>
-              ))}
-            </ul>
-            <button onClick={handleSend} disabled={isSendDisabled}>Send</button>
-          </div>
-        </Answers>
-        <button onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
-          Back
-        </button>
-        <span>
-        {currentQuestionIndex + 1}/{questions.length}
-      </span>
-        <button onClick={handleNextQuestion} disabled={isSendDisabled || currentQuestionIndex === questions.length - 1}>
-        Next
-      </button>
-    </Wrapper>
-  );
-};
+        <Wrapper>
+            <Header>{title}</Header>
+            <Answers>
+                <div key={id}>
+                    <h3>{currentQuestionTitle}</h3>
+                    <ul>
+                        {answers.map(({ id, name, value }) => (
+                            <li key={id}>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name={`answer-${id}`}
+                                        value={value}
+                                        checked={selectedAnswer === value}
+                                        onChange={handleAnswerChange}
+                                    />
+                                    {name}
+                                </label>
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={handleSend} disabled={isSendDisabled}>
+                        Send
+                    </button>
+                </div>
+            </Answers>
+            <Pagination
+                setCurrentQuestionIndex={setCurrentQuestionIndex}
+                setSelectedAnswer={setSelectedAnswer}
+                currentQuestionIndex={currentQuestionIndex}
+                isSendDisabled={isSendDisabled}
+                totalQuestions={questions.length}
+            />
+        </Wrapper>
+    )
+}
