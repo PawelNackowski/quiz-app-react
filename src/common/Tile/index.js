ca815ffa2c17c6f31f7b6cc8279e337f03fb92a8
Paweł <pawelnackowski@gmail.com>
2023-06-24 09:30:34 +0200

    added check correct answer

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -20,46 +20,51 @@ export const Tile = ({ title }) => {
     setSelectedAnswer(event.target.value);
   };
 
+  const checkAnswer = (answer) => {
+    return (
+        answer === questions[currentQuestionIndex].correctAnswer ? "1" : "0"
+    );
+  }
   const handleSend = () => {
-    console.log("Selected Answer:", selectedAnswer);
+    console.log("Selected Answer:", checkAnswer(selectedAnswer));
   };
 
-  const { id, title: currentQuestionTitle, answers } = questions[currentQuestionIndex];
+  const {id, title: currentQuestionTitle, answers} = questions[currentQuestionIndex];
 
   const isSendDisabled = selectedAnswer === null;
 
   return (
-    <Wrapper>
-      <Header>{title}</Header>
-      <Answers>
-        <div key={id}>
-          <h3>{currentQuestionTitle}</h3>
-          <ul>
-            {answers.map(({ id, name, value }) => (
-              <li key={id}>
-                <label>
-                  <input
-                    type="radio"
-                    name={`answer-${id}`}
-                    value={value}
-                    checked={selectedAnswer === value}
-                    onChange={handleAnswerChange}
-                  />
-                  {name}
-                </label>
-              </li>
-            ))}
-          </ul>
-          <button onClick={handleSend} disabled={isSendDisabled}>Send</button>
-        </div>
-      </Answers>
-      <button onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
-        Back
-      </button>
-      <span>
+      <Wrapper>
+        <Header>{title}</Header>
+        <Answers>
+          <div key={id}>
+            <h3>{currentQuestionTitle}</h3>
+            <ul>
+              {answers.map(({id, name, value}) => (
+                  <li key={id}>
+                    <label>
+                      <input
+                          type="radio"
+                          name={`answer-${id}`}
+                          value={value}
+                          checked={selectedAnswer === value}
+                          onChange={handleAnswerChange}
+                      />
+                      {name}
+                    </label>
+                  </li>
+              ))}
+            </ul>
+            <button onClick={handleSend} disabled={isSendDisabled}>Send</button>
+          </div>
+        </Answers>
+        <button onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
+          Back
+        </button>
+        <span>
         {currentQuestionIndex + 1}/{questions.length}
       </span>
-      <button onClick={handleNextQuestion} disabled={isSendDisabled || currentQuestionIndex === questions.length - 1}>
+        <button onClick={handleNextQuestion} disabled={isSendDisabled || currentQuestionIndex === questions.length - 1}>
         Next
       </button>
     </Wrapper>
