368d440668cfb871c3bbead8beb5242eb048c7bb
Paweł <pawelnackowski@gmail.com>
2023-06-24 09:30:09 +0200

    added correct answer

--- src/features/answers.js
+++ src/features/answers.js
@@ -1,80 +1,83 @@
 export const questions = [
-    {
+  {
+    id: 1,
+    title: '1. What is the capital of Poland?',
+    answers: [
+      {
         id: 1,
-        title: "1. What is the capital of Poland?",
-        answers: [
-            {
-                id: 1,
-                name: "Warsaw",
-                value: "Warsaw",
-            },
-            {
-                id: 2,
-                name: "Cracow",
-                value: "Cracow",
-            },
-            {
-                id: 3,
-                name: "Gdansk",
-                value: "Gdansk",
-            },
-            {
-                id: 4,
-                name: "Wroclaw",
-                value: "Wroclaw",
-            },
-        ],
-    },
-    {
+        name: 'Warsaw',
+        value: 'Warsaw',
+      },
+      {
         id: 2,
-        title: "2. What is the capital of Germany?",
-        answers: [
-            {
-                id: 1,
-                name: "Berlin",
-                value: "Berlin",
-            },
-            {
-                id: 2,
-                name: "Munich",
-                value: "Munich",
-            },
-            {
-                id: 3,
-                name: "Hamburg",
-                value: "Hamburg",
-            },
-            {
-                id: 4,
-                name: "Frankfurt",
-                value: "Frankfurt",
-            },
-        ],
-    },
-    {
+        name: 'Cracow',
+        value: 'Cracow',
+      },
+      {
         id: 3,
-        title: "3. What is the capital of France?",
-        answers: [
-            {
-                id: 1,
-                name: "Paris",
-                value: "Paris",
-            },
-            {
-                id: 2,
-                name: "Lyon",
-                value: "Lyon",
-            },
-            {
-                id: 3,
-                name: "Marseille",
-                value: "Marseille",
-            },
-            {
-                id: 4,
-                name: "Toulouse",
-                value: "Toulouse",
-            },
-        ],
-    },
-    ];
+        name: 'Gdansk',
+        value: 'Gdansk',
+      },
+      {
+        id: 4,
+        name: 'Wroclaw',
+        value: 'Wroclaw',
+      },
+    ],
+    correctAnswer: 'Warsaw',
+  },
+  {
+    id: 2,
+    title: '2. What is the capital of Germany?',
+    answers: [
+      {
+        id: 1,
+        name: 'Berlin',
+        value: 'Berlin',
+      },
+      {
+        id: 2,
+        name: 'Munich',
+        value: 'Munich',
+      },
+      {
+        id: 3,
+        name: 'Hamburg',
+        value: 'Hamburg',
+      },
+      {
+        id: 4,
+        name: 'Frankfurt',
+        value: 'Frankfurt',
+      },
+    ],
+    correctAnswer: 'Berlin',
+  },
+  {
+    id: 3,
+    title: '3. What is the capital of France?',
+    answers: [
+      {
+        id: 1,
+        name: 'Paris',
+        value: 'Paris',
+      },
+      {
+        id: 2,
+        name: 'Lyon',
+        value: 'Lyon',
+      },
+      {
+        id: 3,
+        name: 'Marseille',
+        value: 'Marseille',
+      },
+      {
+        id: 4,
+        name: 'Toulouse',
+        value: 'Toulouse',
+      },
+    ],
+    correctAnswer: 'Paris',
+  },
+]
