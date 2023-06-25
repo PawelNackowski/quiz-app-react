bae33d2845d308093988fce352fb2af46ae13014
Paweł <pawelnackowski@gmail.com>
2023-06-25 12:02:33 +0200

    created PageStart component

---
+++ src/features/PageStart/index.js
@@ -0,0 +1,10 @@
+import { StyledLink } from "../styled";
+
+export const PageStart = () => {
+  return (
+      <>
+          <div>Hello !</div>
+          <StyledLink to="/question">start</StyledLink>
+      </>
+  );
+}
