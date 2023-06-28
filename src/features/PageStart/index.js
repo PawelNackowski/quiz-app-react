58e20ad511b1f30a1135646eb300ded68c375db8
Paweł <pawelnackowski@gmail.com>
2023-06-28 12:17:25 +0200

    changed render PageStart

--- src/features/PageStart/index.js
+++ src/features/PageStart/index.js
@@ -1,10 +1,19 @@
-import { StyledLink } from "../styled";
+import { Tile } from '../../common/Tile';
+import { Position, StyledLink } from '../styled';
 
 export const PageStart = () => {
-  return (
-      <>
-          <div>Hello !</div>
-          <StyledLink to="/question/:questionId">start</StyledLink>
-      </>
-  );
-}
+    return (
+        <>
+            <Position>
+                <Tile
+                    title={'Quiz App'}
+                    content={
+                        <StyledLink to="/question/:questionId">
+                            start
+                        </StyledLink>
+                    }
+                ></Tile>
+            </Position>
+        </>
+    );
+};
