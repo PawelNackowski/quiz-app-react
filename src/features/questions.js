f17da280cd4d2c51eb2c67fcb3a3d13e7163d67b
Paweł <pawelnackowski@gmail.com>
2023-06-29 11:03:07 +0200

    changed questions

--- src/features/questions.js
+++ src/features/questions.js
@@ -1,110 +1,261 @@
 export const questions = [
-  {
-    id: 1,
-    title: '1. What is the capital of Poland?',
-    answers: [
-      {
+    {
         id: 1,
-        name: 'Warsaw',
-        value: 'Warsaw',
-      },
-      {
+        title: '1. Jaki jest największy park narodowy w Polsce?',
+        answers: [
+            {
+                id: 1,
+                name: 'Białowieski Park Narodowy',
+                value: 'Białowieski Park Narodowy',
+            },
+            {
+                id: 2,
+                name: 'Tatrzański Park Narodowy',
+                value: 'Tatrzański Park Narodowy',
+            },
+            {
+                id: 3,
+                name: 'Wigierski Park Narodowy',
+                value: 'Wigierski Park Narodowy',
+            },
+            {
+                id: 4,
+                name: ' Biebrzański Park Narodowy',
+                value: ' Biebrzański Park Narodowy',
+            },
+        ],
+        correctAnswer: 'Biebrzański Park Narodowy',
+    },
+    {
         id: 2,
-        name: 'Cracow',
-        value: 'Cracow',
-      },
-      {
+        title: '2.Gdzie znajduje się największy zamek w Polsce pod względem powierzchni?',
+        answers: [
+            {
+                id: 1,
+                name: 'Wawel',
+                value: 'Wawel',
+            },
+            {
+                id: 2,
+                name: 'Książ',
+                value: 'Książ',
+            },
+            {
+                id: 3,
+                name: 'Malbork',
+                value: 'Malbork',
+            },
+            {
+                id: 4,
+                name: 'Moszna',
+                value: 'Moszna',
+            },
+        ],
+        correctAnswer: 'Malbork',
+    },
+    {
         id: 3,
-        name: 'Gdansk',
-        value: 'Gdansk',
-      },
-      {
+        title: '3. Które miasto w Polsce jest uznawane za stolicę kultury i nauki?',
+        answers: [
+            {
+                id: 1,
+                name: 'Warszawa',
+                value: 'Warszara',
+            },
+            {
+                id: 2,
+                name: 'Kraków',
+                value: 'Kraków',
+            },
+            {
+                id: 3,
+                name: 'Wrocław',
+                value: 'Wrocław',
+            },
+            {
+                id: 4,
+                name: 'Gdańsk',
+                value: 'Gdańsk',
+            },
+        ],
+        correctAnswer: 'Kraków',
+    },
+    {
         id: 4,
-        name: 'Wroclaw',
-        value: 'Wroclaw',
-      },
-    ],
-    correctAnswer: 'Warsaw',
-  },
-  {
-    id: 2,
-    title: '2. What is the capital of Germany?',
-    answers: [
-      {
-        id: 1,
-        name: 'Berlin',
-        value: 'Berlin',
-      },
-      {
-        id: 2,
-        name: 'Munich',
-        value: 'Munich',
-      },
-      {
-        id: 3,
-        name: 'Hamburg',
-        value: 'Hamburg',
-      },
-      {
-        id: 4,
-        name: 'Frankfurt',
-        value: 'Frankfurt',
-      },
-    ],
-    correctAnswer: 'Berlin',
-  },
-  {
-    id: 3,
-    title: '3. What is the capital of France?',
-    answers: [
-      {
-        id: 1,
-        name: 'Paris',
-        value: 'Paris',
-      },
-      {
-        id: 2,
-        name: 'Lyon',
-        value: 'Lyon',
-      },
-      {
-        id: 3,
-        name: 'Marseille',
-        value: 'Marseille',
-      },
-      {
-        id: 4,
-        name: 'Toulouse',
-        value: 'Toulouse',
-      },
-    ],
-    correctAnswer: 'Paris',
-  },
-  {
-    id: 4,
-    title: '4. What is the capital of Spain?',
-    answers: [
-      {
-        id: 1,
-        name: 'Madrid',
-        value: 'Madrid',
-      }, 
-      {
-        id: 2,
-        name: 'Barcelona',
-        value: 'Barcelona',
-      },
-      {
-        id: 3,
-        name: 'Valencia',
-        value: 'Valencia',
-      },
-      {
-        id: 4,
-        name: 'Seville',
-        value: 'Seville',
-      },
-    ],
-    correctAnswer: 'Madrid',
-  },
-]
+        title: '4.Jakie jest najgłębsze naturalne jezioro w Polsce?',
+        answers: [
+            {
+                id: 1,
+                name: 'Jezioro Hańcza',
+                value: 'Jezioro Hańcza',
+            },
+            {
+                id: 2,
+                name: 'Jezioro Mamry',
+                value: 'Jezioro Mamry',
+            },
+            {
+                id: 3,
+                name: 'Jezioro Śniardwy',
+                value: 'Jezioro Śniardwy',
+            },
+            {
+                id: 4,
+                name: 'Jezioro Roś',
+                value: 'Jezioro Roś',
+            },
+        ],
+        correctAnswer: 'Jezioro Hańcza',
+    },
+    {
+        id: 5,
+        title: 'Gdzie znajduje się najniżej położony punkt pod poziomem morza?',
+        answers: [
+            {
+                id: 1,
+                name: 'Pacanowo',
+                value: 'Pacanowo',
+            },
+            {
+                id: 2,
+                name: 'Elbląg',
+                value: 'Elbląg',
+            },
+            {
+                id: 3,
+                name: 'Gdańsk',
+                value: 'Gdańsk',
+            },
+            {
+                id: 4,
+                name: 'Żuławy Wiślane',
+                value: 'Żuławy Wiślane',
+            },
+        ],
+        correctAnswer: 'Żuławy Wiślane',
+    },
+    {
+        id: 6,
+        title: '6. Które z wymienionych miast jest najstarsze?',
+        answers: [
+            {
+                id: 1,
+                name: 'Lwówek',
+                value: 'Lwówek',
+            },
+            {
+                id: 2,
+                name: 'Złotoryja',
+                value: 'Złotoryja',
+            },
+            {
+                id: 3,
+                name: 'Nysa',
+                value: 'Nysa',
+            },
+            {
+                id: 4,
+                name: 'Ople',
+                value: 'Opole',
+            },
+        ],
+        correctAnswer: 'Złotoryje',
+    },
+    {
+        id: 7,
+        title: 'Które wydarzenie miało miejsce w Polsce w 1989 roku?',
+        answers: [
+            {
+                id: 1,
+                name: 'Powstanie Warszawskie',
+                value: 'Powstanie Warszawskie',
+            },
+            {
+                id: 2,
+                name: 'Wybó Karola Wojtyły na papieża',
+                value: 'Wybó Karola Wojtyły na papieża',
+            },
+            {
+                id: 3,
+                name: 'Pierwsze częściowo wolne wybory parlamentarne po okresie komunizmu',
+                value: 'Pierwsze częściowo wolne wybory parlamentarne po okresie komunizmu',
+            },
+            {
+                id: 4,
+                name: 'Zakończenie II wojny światowej',
+                value: 'Zakończenie II wojny światowej',
+            },
+        ],
+        correctAnswer:
+            'Pierwsze częściowo wolne wybory parlamentarne po okresie komunizmu',
+    },
+    {
+        id: 8,
+        title: 'Jak nazywa się najwyższy szczyt w Polsce?',
+        answers: [
+            {
+                name: 'Śniezka',
+                value: 'Śniezka',
+            },
+            {
+                name: 'Rysy',
+                value: 'Rysy',
+            },
+            {
+                name: 'Giewont',
+                value: 'Giewont',
+            },
+            {
+                name: 'Kasprowy Wierch',
+                value: 'Kasprowy Wierch',
+            },
+        ],
+        correctAnswer: 'Rysy',
+    },
+    {
+        id: 9,
+        title: 'Które z wymienionych miast jest stolicą Polski?',
+        answers: [
+            {
+                name: 'Kraków',
+                value: 'Kraków',
+            },
+            {
+                name: 'Warszawa',
+                value: 'Warszawa',
+            },
+            {
+                name: 'Gdańsk',
+                value: 'Gdańsk',
+            },
+            {
+                name: 'Wrocław',
+                value: 'Wrocław',
+            },
+        ],
+        correctAnswer: 'Warszawa',
+    },
+    {
+        id: 10,
+        title: 'Gdzie znajduje się Sanktuarium Maryjne w Polsce, które jest jednym z najważniejszych miejsc pielgrzymkowych w Europie?',
+        answers: [
+            {
+                name: 'W Częstochowie',
+                value: 'W Częstochowie',
+            },
+            {
+                name: 'W Jasnej górze',
+                value: 'W Jasnej górze',
+            },
+            {
+                name: 'W Krakowie',
+                value: 'W Krakowie',
+            },
+            {
+                name: 'W Warszawie',
+                value: 'W Warszawie',
+            },
+        ],
+        correctAnswer: 'W Jasnej górze',
+    },
+]; 
