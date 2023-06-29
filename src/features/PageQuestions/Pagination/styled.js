041184e9aa75dddcd4604b5f704200f4be6b8d41
Paweł <pawelnackowski@gmail.com>
2023-06-29 12:01:08 +0200

    styled button and wrapper

---
+++ src/features/PageQuestions/Pagination/styled.js
@@ -0,0 +1,33 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Button = styled.button`
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: transparent;
+color: ${({theme}) => theme.colors.white};
+border: 2px solid ${({theme}) => theme.colors.neonGreen};
+border-radius: 50px;
+font-size: 18px;
+padding: 8px;
+margin: 10px;
+width: 120px;
+transition: 0.3s;
+
+&:hover {
+  transform: scale(1.1);
+    border: 2px solid ${({theme}) => theme.colors.blue};
+    color: ${({theme}) => theme.colors.blue};
+}
+&:active{
+  transform: scale(0.9);
+  filter: brightness(200%);
+}
+`;
