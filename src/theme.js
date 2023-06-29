334627cadaac3dbf82458c70eea7b6cb9344538e
Paweł <pawelnackowski@gmail.com>
2023-06-29 11:59:25 +0200

    added colors neonGreen and blue

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
-export const theme= {
-  colors: {
-    white: '#dddd',
-  }
-
-}
+export const theme = {
+    colors: {
+        white: '#dddd',
+        neonGreen: '#2bd1bb',
+        blue: '#0066cc',
+    },
+};
