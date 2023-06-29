2aa4ca7dd573b3eb9f85dff41ad74bcf65595b36
Paweł <pawelnackowski@gmail.com>
2023-06-29 12:03:33 +0200

    changed welcome text

--- src/features/PageStart/index.js
+++ src/features/PageStart/index.js
@@ -9,7 +9,16 @@ export const PageStart = () => {
                     title={'Quiz App'}
                     extraContent={
                         <>
-                        hello to quiz
+                            <p>
+                                Witaj! Czy jesteś gotowy/a na sprawdzenie swojej
+                                wiedzy o Polsce? Ten quiz zawiera różnorodne
+                                pytania dotyczące ciekawostek związanych z
+                                naszym pięknym krajem.
+                            </p>
+                            <p>
+                                Przygotuj się na wyzwanie i zobacz, ile wiesz o
+                                Polsce!
+                            </p>
                         </>
                     }
                     content={
