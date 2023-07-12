7dff1ef80e0acbfb50ed34ee584add652408018e
Paweł <pawelnackowski@gmail.com>
2023-07-12 15:55:36 +0200

    styled wrapper and list

--- src/features/PageQuestions/styled.js
+++ src/features/PageQuestions/styled.js
@@ -1,7 +1,12 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    max-width: 400px;
   `
+
+  export const List = styled.li`
+    list-style: none;
+    `
