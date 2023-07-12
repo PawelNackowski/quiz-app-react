b08830ded9b141da3fb720b6e5d6ee3f0e600926
Paweł <pawelnackowski@gmail.com>
2023-07-12 15:52:25 +0200

    added styled wrapper

---
+++ src/features/PageEnd/styled.js
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div`
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items: center;
+font-size: 50px;
+padding: 22px;
+gap: 12px;
+`
