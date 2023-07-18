0bdb850d5693a749df7b094a5fa7fd88fe0638ee
Paweł <pawelnackowski@gmail.com>
2023-07-18 22:18:00 +0200

    added breakpoints to theme

--- src/theme.js
+++ src/theme.js
@@ -4,4 +4,9 @@ export const theme = {
         neonGreen: '#2bd1bb',
         blue: '#0066cc',
     },
+    breakpoints: {
+        tablet: 1024,
+        mobileMax: 767,
+        mobileMin: 480,
+    },
 };
