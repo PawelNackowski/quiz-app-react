a1aad22ea098dbb8b878a0a1b4947db73ac0ad71
Paweł <pawelnackowski@gmail.com>
2023-07-18 21:52:34 +0200

    added sound clicked on send, and relocated button

--- src/features/PageQuestions/index.js
+++ src/features/PageQuestions/index.js
@@ -4,6 +4,7 @@ import { Pagination } from './Pagination';
 import { PageEnd } from '../PageEnd';
 import { List, Wrapper } from './styled';
 import { Button } from './Pagination/styled';
+import { click } from '../clickSounds';
 
 export const PageQuestions = () => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -26,11 +27,11 @@ export const PageQuestions = () => {
         if (answerResult === 1) {
             setCorrectAnswersCount((prevCount) => prevCount + 1);
         }
-
         if (currentQuestionIndex === questions.length - 1) {
             setShowResult(true);
         } else {
             setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+            click();
         }
     };
 
@@ -43,61 +44,59 @@ export const PageQuestions = () => {
     } = questions[currentQuestionIndex]
 
     return (
-        <>
-            {!showResult ? (
-                <>
-                    <Wrapper>
-                        <div key={id}>
-                            <h3>{id}. {currentQuestionTitle}</h3>
-                            <ul>
-                                {answers.map(({ id, name, value }) => (
-                                    <List key={id}>
-                                        <label>
-                                            <input
-                                                type="radio"
-                                                name={`answer-${id}`}
-                                                value={value}
-                                                checked={
-                                                    selectedAnswer === value
-                                                }
-                                                onChange={handleAnswerChange}
-                                            />
-                                            {name}
-                                        </label>
-                                    </List>
-                                ))}
-                            </ul>
-                            <Button
-                                onClick={handleSend}
-                                disabled={
-                                    isSendDisabled ||
-                                    currentQuestionIndex !==
-                                        questions.length - 1
-                                }
-                            >
-                                Send
-                            </Button>
-                        </div>
-                    </Wrapper>
-                    <Pagination
-                        setCurrentQuestionIndex={setCurrentQuestionIndex}
-                        setSelectedAnswer={setSelectedAnswer}
-                        currentQuestionIndex={currentQuestionIndex}
-                        isSendDisabled={isSendDisabled}
-                        totalQuestions={questions.length}
-                        correctAnswersCount={correctAnswersCount}
-                        handleSend={handleSend}
-                        currentQuestionId={id}
-                    />
-                </>
-            ) : (
-                <div>
-                    <PageEnd
-                        correctAnswersCount={correctAnswersCount}
-                        currentQuestionIndex={currentQuestionIndex}
-                    />
-                </div>
-            )}
-        </>
+      <>
+        {!showResult ? (
+          <>
+            <Wrapper>
+              <div key={id}>
+                <h3>
+                  {id}. {currentQuestionTitle}
+                </h3>
+                <ul>
+                  {answers.map(({ id, name, value }) => (
+                    <List key={id}>
+                      <label>
+                        <input
+                          type="radio"
+                          name={`answer-${id}`}
+                          value={value}
+                          checked={selectedAnswer === value}
+                          onChange={handleAnswerChange}
+                        />
+                        {name}
+                      </label>
+                    </List>
+                  ))}
+                </ul>
+              </div>
+            </Wrapper>
+            <Pagination
+              setCurrentQuestionIndex={setCurrentQuestionIndex}
+              setSelectedAnswer={setSelectedAnswer}
+              currentQuestionIndex={currentQuestionIndex}
+              isSendDisabled={isSendDisabled}
+              totalQuestions={questions.length}
+              correctAnswersCount={correctAnswersCount}
+              handleSend={handleSend}
+              currentQuestionId={id}
+            />
+            <Button $send
+              onClick={handleSend}
+              disabled={
+                isSendDisabled || currentQuestionIndex !== questions.length - 1
+              }
+            >
+              Send
+            </Button>
+          </>
+        ) : (
+          <div>
+            <PageEnd
+              correctAnswersCount={correctAnswersCount}
+              currentQuestionIndex={currentQuestionIndex}
+            />
+          </div>
+        )}
+      </>
     );
 };
