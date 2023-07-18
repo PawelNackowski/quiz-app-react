d155aba0928ec73aafa7f71ba21b0f2d64347d25
Paweł <pawelnackowski@gmail.com>
2023-07-18 21:53:10 +0200

    added props to button disabled

--- src/features/PageQuestions/Pagination/styled.js
+++ src/features/PageQuestions/Pagination/styled.js
@@ -7,32 +7,33 @@ align-items: center;
 `;
 
 export const Button = styled.button`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: transparent;
-    color: ${({ theme }) => theme.colors.white};
-    border: 2px solid ${({ theme }) => theme.colors.neonGreen};
-    border-radius: 50px;
-    font-size: 18px;
-    padding: 8px;
-    margin: 10px;
-    width: 120px;
-    transition: 0.3s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  color: ${({ theme }) => theme.colors.white};
+  border: 2px solid ${({ theme }) => theme.colors.neonGreen};
+  border-radius: 50px;
+  font-size: 18px;
+  padding: 8px;
+  margin: 10px;
+  width: 120px;
+  transition: 0.3s;
 
-    &:hover {
-        transform: scale(1.1);
-        border: 2px solid ${({ theme }) => theme.colors.blue};
-        color: ${({ theme }) => theme.colors.blue};
-        cursor: pointer;
-    }
-    &:active {
-        transform: scale(0.9);
-        filter: brightness(200%);
-    }
+  &:hover {
+    transform: scale(1.1);
+    border: 2px solid ${({ theme }) => theme.colors.blue};
+    color: ${({ theme }) => theme.colors.blue};
+    cursor: pointer;
+  }
+  &:active {
+    transform: scale(0.9);
+    filter: brightness(200%);
+  }
 
-    &:disabled {
-        border: 2px solid gray;
-        background-color: gray;
-    }
+  &:disabled {
+    display: ${(props) => (props.$send ? 'none' : 'flex')};
+    border: 2px solid gray;
+    background-color: gray;
+  }
 `;
