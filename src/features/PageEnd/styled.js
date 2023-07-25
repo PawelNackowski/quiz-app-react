0aa1be9d8cc386d1c2a5bde9cc36da75399239fd
Paweł <pawelnackowski@gmail.com>
2023-07-25 18:43:35 +0200

    added media query

--- src/features/PageEnd/styled.js
+++ src/features/PageEnd/styled.js
@@ -7,4 +7,12 @@ justify-content: center;
 align-items: center;
 font-size: 35px;
 gap: 12px;
+
+@media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 32px;
+};
+
+@media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
+    font-size: 26px;
+}
 `
