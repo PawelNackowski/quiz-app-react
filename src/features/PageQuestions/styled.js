c07470c43265ad008ae78ec7fe7f7f9dbab4a170
Paweł <pawelnackowski@gmail.com>
2023-07-25 18:45:47 +0200

    deleted max-width on wrapper and added text-aling center to wrapper

--- src/features/PageQuestions/styled.js
+++ src/features/PageQuestions/styled.js
@@ -1,12 +1,12 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 300px;
-  `
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`
 
   export const List = styled.li`
     list-style: none;
