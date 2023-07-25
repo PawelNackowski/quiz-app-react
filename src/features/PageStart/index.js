a0220a4ba4763f28868ca1096aa227e8ce721aad
Paweł <pawelnackowski@gmail.com>
2023-07-25 18:47:53 +0200

    changed content and added wrapper to tile

--- src/features/PageStart/index.js
+++ src/features/PageStart/index.js
@@ -1,31 +1,29 @@
 import { Tile } from '../../common/Tile';
+import { Wrapper } from '../PageQuestions/styled';
 import {click} from '../clickSounds';
 import { StyledLink } from '../styled';
 
 export const PageStart = () => {
     return (
-      <>
-
-          <Tile
-            title={'Quiz App'}
-            extraContent={
-              <>
-                <p>
-                  Witaj! Czy jesteś gotowy/a na sprawdzenie swojej wiedzy o
-                  Polsce? Ten quiz zawiera różnorodne pytania dotyczące
-                  ciekawostek związanych z naszym pięknym krajem.
-                </p>
-                <p>Przygotuj się na wyzwanie i zobacz, ile wiesz o Polsce!</p>
-              </>
-            }
-            content={
-              <>
-                <StyledLink onClick={click} to="/question/:questionId">
-                  start
-                </StyledLink>
-              </>
-            }
-          ></Tile>
-      </>
+      <Tile
+        title={'Quiz o Polsce'}
+        content={
+          <>
+            <Wrapper>
+              Witaj!<br/> 
+              Czy jesteś gotowy/a na sprawdzenie swojej
+              <br />
+              wiedzy o Polsce? Ten quiz zawiera różnorodne
+              <br />
+              pytania dotyczące ciekawostek związanych
+              <br />z naszym pięknym krajem.
+            <p>Przygotuj się na wyzwanie i zobacz, ile wiesz o Polsce!</p>
+            <StyledLink onClick={click} to="/question/:questionId">
+              start
+            </StyledLink>
+            </Wrapper>
+          </>
+        }
+      ></Tile>
     );
 };
